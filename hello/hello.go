096161ad392c0ec0efea589826225252271c6798
mackoko <maciekrz12@gmail.com>
2020-10-01 16:40:38 +0200

    first module complete

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	v := 10
+
+	p1 := &v
+	p2 := &p1
+
+	fmt.Println(p2)
+	fmt.Println(*p2)
+	fmt.Println(**p2)
+
+	fmt.Println(p1)
+	fmt.Println(*p1)
+	// fmt.Println(**p1) invalid indirect compilation error
+
+	fmt.Println(v)
+	*p1 = 20
+	fmt.Println(v)
+	**p2 = 30
+	fmt.Println(v)
+
+	fmt.Println("Hello World")
 }
--- number_types/number_types.go
+++ number_types/number_types.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
 
 func main() {
 
-	var i int8 = 20
+	var i uint8 = 255
 	var f float32 = 5.6
 	fmt.Println(float32(i) + f)
+
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	// The expression T(v) converts the value v to the type T.
+	var x, y int = 3, 4
+	var fl float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(fl)
+	fmt.Println(x, y, z)
 
 }
